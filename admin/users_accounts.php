18bb0a3e06ff361558d04caccf393c674f01907e
abdelmajied94abusuliman <115414116+abdelmajied94abusuliman@users.noreply.github.com>
2023-01-01 13:57:47 +0300

    dashboard

--- admin/users_accounts.php
+++ admin/users_accounts.php
@@ -16,12 +16,8 @@ if(isset($_GET['delete'])){
    $delete_user->execute([$delete_id]);
    $delete_orders = $conn->prepare("DELETE FROM `orders` WHERE user_id = ?");
    $delete_orders->execute([$delete_id]);
-   $delete_messages = $conn->prepare("DELETE FROM `messages` WHERE user_id = ?");
-   $delete_messages->execute([$delete_id]);
    $delete_cart = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
    $delete_cart->execute([$delete_id]);
-   $delete_wishlist = $conn->prepare("DELETE FROM `wishlist` WHERE user_id = ?");
-   $delete_wishlist->execute([$delete_id]);
    header('location:users_accounts.php');
 }
 
