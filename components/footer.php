bb2fdd1967435bc0a87ed821e79ee7aeb7339185
unknown <nooralkassasbeh.com>
2023-01-01 17:50:42 +0300

    footer

--- components/footer.php
+++ components/footer.php
@@ -1,37 +1,37 @@
-<footer class="footer">
+<footer class="footer" style="background-color:black;"> 
 
    <section class="grid">
 
       <div class="box">
-         <h3>quick links</h3>
-         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
-         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
-         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
-         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+         <h3 style="color:white">quick links</h3>
+         <a href="home.php" style="color:white"> <i class="fas fa-angle-right" style="color:white" ></i> Home</a>
+         <a href="about.php" style="color:white"> <i class="fas fa-angle-right" style="color:white"></i> About</a>
+         <a href="shop.php" style="color:white"> <i class="fas fa-angle-right" style="color:white"></i> Shop</a>
+         <a href="contact.php" style="color:white"> <i class="fas fa-angle-right" style="color:white"></i> Contact</a>
       </div>
 
       <div class="box">
-         <h3>extra links</h3>
-         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
-         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
-         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
-         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+         <h3 style="color:white">extra links</h3>
+         <a href="user_login.php"style="color:white"> <i class="fas fa-angle-right"style="color:white"></i> Login</a>
+         <a href="user_register.php"style="color:white"> <i class="fas fa-angle-right"style="color:white"></i> Register</a>
+         <a href="cart.php"style="color:white"> <i class="fas fa-angle-right"style="color:white"></i> Cart</a>
+         <a href="orders.php"style="color:white"> <i class="fas fa-angle-right"style="color:white"></i> Orders</a>
       </div>
 
       <div class="box">
-         <h3>contact us</h3>
-         <a href="tel:1234567890"><i class="fas fa-phone"></i> +123 456 7899</a>
-         <a href="tel:11122233333"><i class="fas fa-phone"></i> +111 222 3333</a>
-         <a href="mailto:shaikh@gmail.com" target="_blank"><i class="fas fa-envelope"></i> Art Hand Craft@gmail.com</a>
-         <a href="https://www.google.com/myplace" target="_blank"><i class="fas fa-map-marker-alt"></i> Aqaba, Jordan - 123456 </a>
+         <h3 style="color:white">contact us</h3>
+         <a href="tel:1234567890"style="color:white"><i class="fas fa-phone"style="color:white"></i> +123 456 7899</a>
+         <a href="tel:11122233333" style="color:white"><i class="fas fa-phone" style="color:white"></i> +111 222 3333</a>
+         <a href="mailto:shaikh@gmail.com" target="_blank"style="color:white"><i class="fas fa-envelope"style="color:white"></i> Art Hand Craft@gmail.com</a>
+         <a href="https://www.google.com/myplace" target="_blank"style="color:white"><i class="fas fa-map-marker-alt"style="color:white"></i> Aqaba, Jordan - 123456 </a>
       </div>
 
       <div class="box">
-         <h3>follow us</h3>
-         <a href="https://web.facebook.com/?_rdc=1&_rdr" target="_blank"><i class="fab fa-facebook-f"></i>Facebook</a>
-         <a href="https://twitter.com/i/flow/login" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
-         <a href="https://www.instagram.com/accounts/login/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
-         <a href="https://www.linkedin.com/home" target="_blank" ><i class="fab fa-linkedin"></i>Linkedin</a>
+         <h3 style="color:white">follow us</h3>
+         <a href="https://web.facebook.com/?_rdc=1&_rdr" target="_blank"style="color:white" ><i class="fab fa-facebook-f" style="color:white"></i>Facebook</a>
+         <a href="https://twitter.com/i/flow/login" target="_blank" style="color:white"><i class="fab fa-twitter" style="color:white" ></i>Twitter</a>
+         <a href="https://www.instagram.com/accounts/login/" target="_blank"style="color:white" ><i class="fab fa-instagram" style="color:white" ></i>Instagram</a>
+         <a href="https://www.linkedin.com/home" target="_blank" style="color:white"><i class="fab fa-linkedin" style="color:white"></i>Linkedin</a>
       </div>
 
    </section>
